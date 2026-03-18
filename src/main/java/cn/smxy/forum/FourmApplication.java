ae1fd3f168a5594930d10c513535993d23d4a74a
sgy1205 <2174892027@qq.com>
2026-03-18 13:38:35 +0800

    添加部分修改

--- src/main/java/cn/smxy/forum/FourmApplication.java
+++ src/main/java/cn/smxy/forum/FourmApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableTransactionManagement
 @MapperScan("cn.smxy.forum.mapper")
 @ComponentScan("cn.smxy")
 public class FourmApplication {
